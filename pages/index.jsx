ea04f496dbad6636a3c8c648dd4da3adcee091a9
toony10 <amiranton257@gmail.com>
2023-03-29 21:49:33 +0200

    lol

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import NavBar from "/components/NavBar";
-import Slider from "/components/Slider";
-import About from "/components/About";
-import Products from "/components/products";
-import Location from "/components/Location";
-import Contact from "/components/contact";
+import styles from "../styles/Home.module.css";
+import NavBar from "../components/NavBar";
+import Slider from "../components/Slider";
+import About from "../components/About";
+import Products from "../components/products";
+import Location from "../components/Location";
+import Contact from "../components/contact";
 export default function Home() {
   return (
     <div className='bg-white '>
