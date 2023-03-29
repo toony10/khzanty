d93682440b1023280b7e1bc5a1bcfff204d9b447
toony10 <amiranton257@gmail.com>
2023-03-29 21:56:57 +0200

    lol2

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 function Card() {
   return (
     <>
       <div className='card bg-white w-[200px] h-[350px] m-2 rounded-lg shadow-lg'>
         <div className='top'>
-          <img
+          <Image
+            width={0}
+            height={0}
             className='w-[200px] h-[200px] object-cover  p-2'
             src='https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204'
             alt='img'
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["tailus.io", "tailwindui.com", "assets.goal.com", "mdbcdn.b-cdn.net"],
+    domains: ["tailus.io", "tailwindui.com", "assets.goal.com", "mdbcdn.b-cdn.net", "store.storeimages.cdn-apple.com"],
   },
 };
 
