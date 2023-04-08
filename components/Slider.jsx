f6349cc32f1c5fb88c0278be4d5fad12a0dffa17
toony10 <amiranton257@gmail.com>
2023-04-08 21:49:26 +0200

    edit home

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -20,7 +20,7 @@ const Slider = () => {
           <div className='mt-10 flex items-center justify-center gap-x-6'>
             <a
               href='#products'
-              className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+              className='rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600'>
               ابدأ رحلتك معنا الأن
             </a>
           </div>
