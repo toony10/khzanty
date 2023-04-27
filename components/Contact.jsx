18b4ace03e0e96443a169b65df4563cf4e22f5f8
toony10 <amiranton257@gmail.com>
2023-04-27 18:37:18 +0200

    edit contact form2

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -98,7 +98,7 @@ const Contact = () => {
           </button>
           <div
             id='alert'
-            className='m-auto mb-5 ease-in duration-200  justify-center text-green-600 bg-green-400 w-1/2 items-center rounded-lg bg-success-100 px-6 py-5 text-base text-success-700'
+            className='m-auto mb-5 ease-in duration-200 hidden justify-center text-green-600 bg-green-400 w-1/2 items-center rounded-lg bg-success-100 px-6 py-5 text-base text-success-700'
             role='alert'>
             <span className='w-full justify-center flex'>
               <svg
